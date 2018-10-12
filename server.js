481e66fa44f90b0a135f42a95da14f5106bbd32b
kthoma2484 <35786891+kthoma2484@users.noreply.github.com>
2018-10-11 21:15:15 -0700

    added user auth files

--- server.js
+++ server.js
@@ -3,20 +3,35 @@ const path = require("path");
 const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 3001;
 const app = express();
+const passport = require('passport');
+const flash = require('connect-flash');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
 
 // Requiring our models for syncing
-var db = require("./models");
+var db = require("./server/models");
 
-// Define middleware here
+// pass passport for configuration
+//require('./config/passport')(passport); 
+
+// Set up express application
+app.use(cookieParser()); //read cookies (needed for auth)
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+// require for passport
+app.use(session({ secret: 'tellmewhatyouwantwhatyoureallyreallywant' })); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
+
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
 // Define API routes here
-require("./API-db.js")(app);
+require("./server/API-db.js")(app, passport); // load our routes and pass in our app and fully configured passport
 
 // Send every other request to the React app
 // Define any API routes before this runs
